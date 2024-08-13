42db2f31cbe3b1e80a4d62aa40299f0b1c3a7bac
Celia Ait Ouazzou <celiaaitouazzou@gmail.com>
2024-08-13 15:51:32 -0400

    New File

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat(name)
+{
+    const newArray = [...cats,name];
+    return newArray;
+}
+
+function prependCat(name)
+{
+    const newArray = [name,...cats];
+    return newArray;
+}
+
+function removeLastCat()
+{
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
